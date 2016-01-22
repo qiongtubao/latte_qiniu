759305d096a7312839f2e864d644035beee8be31
qingjiao <qingjiao@localhost.localdomain>
2016-01-22 21:12:19 +0800

    init latte_qiniu

---
+++ index.js
@@ -0,0 +1,8 @@
+(function(define) {'use strict'
+	define("latte_qiniu", ["require", "exports", "module", "window"],
+ 	function(require, exports, module, window) {
+ 		var child = function() {
+ 			
+ 		}
+ 	});
+})(typeof define === "function"? define: function(name, reqs, factory) {factory(require, exports, module); });
